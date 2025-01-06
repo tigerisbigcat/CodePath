dd9b8bbfd996a9e2235937d8f418447669dc1211
Catherine <xiayushan2003@gmail.com>
2025-01-05 17:19:44 -0800

    app

---
+++ Prework/Prework/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Catherine on 1/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
